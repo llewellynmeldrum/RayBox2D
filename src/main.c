d3a09b9e6be47e7784a2517f199015ab3dea7d0a
llewellynmeldrum <llewellyn.meldrum1@students.mq.edu.au>
2025-08-15 16:19:00 +1000

    Added boxes array, translation between world & screen pos, and boxes spawn on tap. Rotation rendering is broken.

--- src/main.c
+++ src/main.c
@@ -10,39 +10,71 @@
 #include "physics_world.h"
 
 
+#define PPM 100.0f
 // Raylib_Helper.c
 char debug_text[512];
 int FrameCount = 1;
 int DebugUpdateRate = 10; 
 float FrameTimeMS = -1.0f; 
 float FrameRate = -1.0f; 
+
 void DrawDebugMenu(float ox, float oy);
 
 
 // SharedHelper.h 
+Vector2 origin = (Vector2){0,0};
 b2WorldId worldId;
-float timeStep;
-int subStepCount;
+float timeStep = 1.0f / 60.0f;
+int subStepCount = 4;
+float gravity_y = -10.f;
 
 Font debugFont;
 Vector2 windowRes;
 
 struct r2d_rect{
     b2BodyId id;
-    b2Vec2 scale;
+    b2Vec2 halfExtents;
 };
 typedef struct r2d_rect Rect;
 
-void DrawRect(Rect rect){
-    b2Transform t = b2Body_GetTransform(rect.id);
-    Rectangle rectangle = (Rectangle){
-        .x = t.p.x,
-        .y = t.p.y,
-        .width = rect.scale.x,
-        .height = rect.scale.y,
+Vector2 worldPosToScreenPos(b2Vec2 worldPos){
+    Vector2 screenPos;
+    screenPos.x = worldPos.x*PPM;
+    screenPos.y = windowRes.y - (worldPos.y*PPM);
+    return screenPos;
+}
+// sy = W - wy*P
+// wy*P = W - sy
+// wy = (W-sy)/P
+b2Vec2 screenPosToWorldPos(float sx, float sy){
+    b2Vec2 worldPos = {
+        worldPos.x = sx/PPM,
+        worldPos.y = (windowRes.y - sy)/PPM,
     };
-    DrawRectanglePro(rectangle, (Vector2){0,0}, b2Rot_GetAngle(t.q), WHITE);
+    return worldPos;
+}
 
+void DrawRect(Rect rect){
+    // box2d expects top left position.
+    // rect.p is the middle
+    //
+    b2Transform tr = b2Body_GetTransform(rect.id);
+    // center = x,y
+    // top left = x-w, y-h
+    b2Vec2 worldPosTopLeft = (b2Vec2){
+        .x = tr.p.x - rect.halfExtents.x,
+        .y = worldPosTopLeft.y = tr.p.y + rect.halfExtents.y
+    };
+    Vector2 rScreenPos = worldPosToScreenPos(worldPosTopLeft);
+    Rectangle screenRec = (Rectangle){
+        .x = rScreenPos.x,
+        .y = rScreenPos.y,
+        .width = (rect.halfExtents.x*2) * PPM,
+        .height = (rect.halfExtents.y*2) * PPM,
+    };
+    printf("    DRAWING RECT [w:%02f, h:%02f] @ (x:%02f, y:%02f)\n",screenRec.width, screenRec.height, screenRec.x, screenRec.y);
+    printf("    REAL RECTPOS [w:%02f, h:%02f] @ (x:%02f, y:%02f)\n",rect.halfExtents.x*2, rect.halfExtents.y*2, tr.p.x, tr.p.y);
+    DrawRectanglePro(screenRec, origin, b2Rot_GetAngle(tr.q), WHITE);
 }
 
 // B2D_Helper.h
@@ -57,17 +89,24 @@ b2WorldId InitWorld(float grav_y){
     return b2CreateWorld(&worldDef);
 }
 
-Rect CreateRect(b2Vec2 pos, b2Vec2 scale, float density, float friction){
+Rect CreateRect(b2Vec2 pos, float width, float height, float density, float friction, bool isDynamic){
     b2BodyDef bodyDef = b2DefaultBodyDef();
-    bodyDef.type = b2_dynamicBody;
+    b2Vec2 halfExtents = (b2Vec2){width/2.0f, height/2.0f};
+
+    if (isDynamic)
+        bodyDef.type = b2_dynamicBody;
+    else 
+        bodyDef.type = b2_staticBody;
+
     bodyDef.position = (b2Vec2){pos.x, pos.y};
     b2BodyId bodyId = b2CreateBody(worldId, &bodyDef);
 
-    b2Polygon dynamicBox = b2MakeBox(scale.x, scale.y);
+    b2Polygon dynamicBox = b2MakeBox(halfExtents.x, halfExtents.y);
     b2ShapeDef shapeDef = b2DefaultShapeDef();
     shapeDef.density = density;
     shapeDef.material.friction = friction; 
-    return (Rect){.id = bodyId, .scale=scale};
+    b2CreatePolygonShape(bodyId, &shapeDef, &dynamicBox);
+    return (Rect){.id = bodyId, .halfExtents=halfExtents};
 }
 
 // ----------------------
@@ -76,16 +115,14 @@ Rect CreateRect(b2Vec2 pos, b2Vec2 scale, float density, float friction){
 
 int main(){
 //b2setup()
-    timeStep = 1.0f / 60.0f;
-    subStepCount = 4;
 
-    worldId = InitWorld(-10.0f);
-    Rect rect = CreateRect(
-        (b2Vec2){400.0f, 200.0f},
-        (b2Vec2){100.0f, 100.0f},
-        1.0f,
-        0.3f
-    );
+    worldId = InitWorld(gravity_y);
+    float side_len = 1.0f;
+    Rect rectangles [100]; 
+    rectangles[0] = CreateRect((b2Vec2){10.0f, 10.0f}, side_len, side_len, 1.0f, 0.3f, true); // isDynamic);
+    rectangles[1]= CreateRect((b2Vec2){0.0f, 2.0f}, 50.0f, 2.0f, 1.0f, 0.3f, false); // isDynamic);
+    int rectCount = 2;
+
 
 
 //raysetup()
@@ -98,16 +135,23 @@ int main(){
 
     while (!WindowShouldClose()) {
         // do physics updates
+        if (IsMouseButtonPressed(MOUSE_BUTTON_LEFT)) {
+            b2Vec2 pos = screenPosToWorldPos(GetMouseX(), GetMouseY());
+            rectangles[rectCount] = CreateRect(pos, side_len, side_len, 1.0f, 0.3f, true); // isDynamic);
+            rectCount++;
+        }
         b2World_Step(worldId, timeStep, subStepCount);
 
         // do draw updates
         BeginDrawing();
         DrawDebugMenu(10,10);
 
-        DrawCircle(GetMouseX(), GetMouseY(), 20.0f, WHITE);
-        DrawRect(rect);
-        ClearBackground(BLACK);
+        //DrawCircle(GetMouseX(), GetMouseY(), 20.0f, WHITE);
 
+        for (int i = 0; i<rectCount; i++)
+            DrawRect(rectangles[i]);
+
+        ClearBackground(BLACK);
 
         if (FrameCount%DebugUpdateRate==0){
             FrameTimeMS = GetFrameTime()*1000.0f;
